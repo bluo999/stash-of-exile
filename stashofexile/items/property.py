87c6abdd2ce3a5365d082c3a06491c2efaca6a12
Brian Luo <brianluo999@gmail.com>
2022-05-12 15:18:30 -0400

    Fix display of certain properties

--- stashofexile/items/property.py
+++ stashofexile/items/property.py
@@ -21,6 +21,10 @@ class Property:
         if self.tooltip is not None:
             return self.tooltip
 
+        if self.name == '':
+            self.tooltip = str(self.values[0][0])
+            return self.tooltip
+
         # Resonator text
         name = self.name
         if '<unmet>' in self.name:
@@ -37,20 +41,21 @@ class Property:
         if obj['inserted'] or not self.values or self.values[0][0] == '':
             # Property without label
             self.tooltip = util.colorize(obj['text'], 'grey')
-        else:
-            tooltip = []
-            tooltip.append(util.colorize(obj['text'] + ': ', 'grey'))
-            first = True
-            for val, valnum in self.values:
-                # Property with label
-                assert isinstance(valnum, int)
-                if not first:
-                    tooltip.append(util.colorize(', ', 'grey'))
-                if valnum not in consts.VALNUM_TO_COLOR:
-                    logger.error('Color not found: %s for text %s', valnum, val)
-                color = consts.VALNUM_TO_COLOR.get(valnum, 'white')
-                tooltip.append(util.colorize(str(val).replace('\n', '<br />'), color))
-                first = False
-            self.tooltip = ''.join(tooltip)
+            return self.tooltip
 
+        tooltip = []
+        tooltip.append(util.colorize(obj['text'] + ': ', 'grey'))
+        first = True
+        for val, valnum in self.values:
+            # Property with label
+            assert isinstance(valnum, int)
+            if not first:
+                tooltip.append(util.colorize(', ', 'grey'))
+            if valnum not in consts.VALNUM_TO_COLOR:
+                logger.error('Color not found: %s for text %s', valnum, val)
+            color = consts.VALNUM_TO_COLOR.get(valnum, 'white')
+            tooltip.append(util.colorize(str(val).replace('\n', '<br />'), color))
+            first = False
+
+        self.tooltip = ''.join(tooltip)
         return self.tooltip
